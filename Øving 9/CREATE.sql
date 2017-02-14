9b1b920d0f4500e5212dab96c00ba403bd7e1556
Trygve Nerland <trygvne@stud.ntnu.no>
2017-02-14 13:42:31 +0100

    øving 9 oppdate

--- Øving 9/CREATE.sql
+++ Øving 9/CREATE.sql
@@ -41,13 +41,15 @@ CREATE TABLE oppdrag (
   kvalifikasjon_id_krav INTEGER,
   start_dato DATE NOT NULL,
   slutt_dato DATE NOT NULL,
+  antall_timer INTEGER NOT NULL,
   kandidat_nr INTEGER,
   
   virkelig_start_dato DATE,
   virkelig_slutt_dato DATE,
   PRIMARY KEY (oppdrag_nr),
   FOREIGN KEY (org_nr) REFERENCES bedrifter(org_nr),
-  FOREIGN KEY (kvalifikasjon_id_krav) REFERENCES kvalifikasjoner(kvalifikasjon_id)
+  FOREIGN KEY (kvalifikasjon_id_krav) REFERENCES kvalifikasjoner(kvalifikasjon_id),
+  FOREIGN KEY (kandidat_nr) REFERENCES kandidater(kandidat_nr)
 );
 
 INSERT INTO kandidater VALUES(12, 'Geir', 'Geirsen', '21233424', 'geir@geir.geir');
@@ -69,7 +71,7 @@ INSERT INTO kandidat_kvalifikasjoner VALUES(2, 2);
 INSERT INTO bedrifter VALUES(123, 'Byggern', '13371337', 'byggern@byggern.no');
 INSERT INTO bedrifter VALUES(145, 'Elkjop', '11242133', 'elkjop@elkjop.no');
 
-INSERT INTO oppdrag VALUES(DEFAULT, 123, 3, DATE('2017-02-05'), DATE('2017-02-08'), null, null, null);
-INSERT INTO oppdrag VALUES(DEFAULT, 123, 3, DATE('2017-01-05'), DATE('2017-01-08'), 12, DATE('2017-01-05'), DATE('2017-01-08'));
-INSERT INTO oppdrag VALUES(DEFAULT, 145, 3, DATE('2017-01-08'), DATE('2017-01-09'), 12, DATE('2017-01-09'), DATE('2017-01-10'));
+INSERT INTO oppdrag VALUES(DEFAULT, 123, 3, DATE('2017-02-05'), DATE('2017-02-08'), DEFAULT, null, null, null);
+INSERT INTO oppdrag VALUES(DEFAULT, 123, 3, DATE('2017-01-05'), DATE('2017-01-08'), 3, 12, DATE('2017-01-05'), DATE('2017-01-08'));
+INSERT INTO oppdrag VALUES(DEFAULT, 145, 3, DATE('2017-01-08'), DATE('2017-01-09'), 5, 12, DATE('2017-01-09'), DATE('2017-01-10'));
 
--- Øving 9/oppg.sql
+++ Øving 9/oppg.sql
@@ -16,6 +16,7 @@ SELECT DISTINCT kandidat_nr, fornavn, etternavn, kvalifikasjon_id, kvalifikasjon
 
 -- 4.
 
+-- ikke bruk det mosteret her, bruk LEFT JOIN istedet!
 SELECT DISTINCT kandidat_nr, fornavn, etternavn, kvalifikasjon_id, kvalifikasjon_navn FROM kandidater NATURAL JOIN kandidat_kvalifikasjoner NATURAL JOIN kvalifikasjoner
 UNION
 SELECT DISTINCT kandidat_nr, fornavn, etternavn, null, null FROM kandidater 
@@ -25,4 +26,4 @@ SELECT DISTINCT kandidat_nr FROM kandidat_kvalifikasjoner
 
 -- 5.
 
-SELECT DISTINCT kandidat_nr, fornavn, etternavn, virkelig_start_dato, virkelig_slutt_dato, oppdrag_nr, bedrift_navn FROM oppdrag NATURAL JOIN kandidater NATURAL JOIN bedrifter WHERE kandidat_nr=12
+SELECT DISTINCT kandidat_nr, fornavn, etternavn, virkelig_start_dato, virkelig_slutt_dato, antall_timer, oppdrag_nr, bedrift_navn FROM oppdrag NATURAL JOIN kandidater NATURAL JOIN bedrifter WHERE kandidat_nr=12
