49b2b88c160620a8803f6cdf1eebef618bdc4202
Trygve Nerland <trygvne@stud.ntnu.no>
2017-03-17 12:40:01 +0100

    Revert "Øving 17"
    This reverts commit 2b2ef104ef9d4de69d9e5a4b125632d3bed2c98e.

--- Øving 16/oppg.sql
+++ Øving 16/oppg.sql
@@ -1,7 +1,6 @@
 -- Oppg 1. a)
 
-SELECT * FROM levinfo NATURAL JOIN ordrehode NATURAL JOIN ordredetalj 
-WHERE levnr = 44;
+SELECT * FROM levinfo NATURAL JOIN ordrehode NATURAL JOIN ordredetalj WHERE levnr = 44;
 
 -- Oppg 1. b)
 
--- Øving17/src/com/trygve/oving17/DyreFabrikk.java
+++
@@ -1,7 +0,0 @@
-package com.trygve.oving17.dyrehage;
-
-/**
- * Created by Trygve on 15.03.2017.
- */
-public class DyreFabrikk {
-}
